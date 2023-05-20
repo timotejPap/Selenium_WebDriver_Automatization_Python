b50f390023bce1ac4a99265aa94602900cdf1a4c
papti <paptimotej@gmail.com>
2023-05-20 06:51:23 +0200

    Par zmien

--- E-Shop_Demoblaze.py
+++ E-Shop_Demoblaze.py
@@ -8,9 +8,11 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
 
-driver.maximize_window()
+class Najdi_element():
+    def klik(self):
+        driver.maximize_window()
+        driver.get("https://www.demoblaze.com/")
+        driver.find_element(By.LINK_TEXT, "Laptops").click()
 
-driver.get("https://www.demoblaze.com/")
-
-price = driver.find_elements(By.CSS_SELECTOR, "body > div:nth-child(6) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > h5:nth-child(2)")
-print(price)
+najdi_link = Najdi_element()
+najdi_link.klik()
--- Locating_Elements_Cookies_Kaufland.py
+++ Locating_Elements_Cookies_Kaufland.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 # Toto ponechá okno otvorené
@@ -21,7 +22,12 @@ time.sleep(3)
 
 cookie = driver.find_element(By.XPATH, "//button[contains(text(),'Povoliť vybrané')]").click()
 time.sleep(3)
-
-#kontakt = driver.find_element(By.CLASS_NAME, "m-navigation-corporate__link").click()
-
 print("Nazov stranky je: ", driver.title)
+
+search = driver.find_element(By.NAME, "search_value")
+time.sleep(5)
+submit = driver.find_element(By.XPATH, "//button[@type='button']//span[@class='svg-icon']//*[name()='svg']")
+ActionChains(driver).send_keys_to_element(search, "slanina").click(submit).perform()
+time.sleep(4)
+slane = driver.find_element(By.XPATH, "//div[contains(text(),'Slané pečivo')]").click()
+#link = driver.find_element(By.LINK_TEXT, "Kariéra v predajniach Kaufland").click()
